32a6c516417063baf84489fde7783bfb50bded4b
RyanPulivarthi <ryanpulivarthi5515@gmail.com>
2024-03-05 15:12:45 +0530

    seond commit

--- src/Factory/Client.java
+++ src/Factory/Client.java
@@ -3,7 +3,7 @@ package Factory;
 public class Client {
     public static void main(String[] args) {
         Flutter flutter=new Flutter();
-        UIFactory factory= flutter.createUIFactory(SupportedPlatforms.Android);
+        UIFactory factory= flutter.createUIFactory(SupportedPlatforms.IOS);
 
         Button button=factory.createButton();
         button.showButton();
